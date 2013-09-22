4c8789747cc470c1ba03ada0a258543009bc6c9f
staneslas <staneslasm@gmail.com>
2013-09-22 19:55:43 +0300

    Hello world

---
+++ src/main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: staneslas
+ * Date: 9/22/13
+ * Time: 7:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class main {
+    public static void main(String... args) {
+        System.out.printf("Hello world!");
+    }
+}
